2ae77ac8761734b33eaecb244facd809f48444ba
Kieran McDonald <kieran.mcdonald@qantas.com.au>
2019-09-13 08:57:28 +1000

    Improve example a bit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
 import React from "react";
 import "./App.css";
+import { retrieveAirports, retrieveAirportInfoByCode } from "./services";
+
+retrieveAirports("sydney").then(airports => {
+  console.log(airports);
+});
+
+retrieveAirportInfoByCode("SYD").then(data => {
+  console.log(data);
+});
 
 function App() {
   return <div>Your app </div>;
